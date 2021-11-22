da04a44b3848c5be6b1599e55149a77f17e7912c
abellasjuan <juanvictoraa@gmail.com>
2021-11-21 22:24:40 -0300

    button done

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -27,8 +27,8 @@ export default function FeaturedMovie({item}){
                     </div>
                     <div className="featured--description">{item.overview}</div>
                     <div className="featured--buttons">
-                        <a href={`/watch/${item.id}`}>►Assistir</a>
-                        <a href={`/list/add/${item.id}`}>+Minha Lista</a>
+                        <a href={`/watch/${item.id}`} className="featured--watchbutton"> ► Assistir </a>
+                        <a href={`/list/add/${item.id}`} className="featured--mylistbutton"> + Minha Lista </a>
                     </div>
                     <div className="featured--genres"><strong>Gêneros: {genres.join(', ')}</strong></div>
                     
